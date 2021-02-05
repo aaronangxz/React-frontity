963899f7df34c97a90ac9bfe2efe6e704f7050b4
aaronangxz <angxze@gmail.com>
2021-02-05 23:42:54 +0800

    First commit

---
+++ frontity.settings.js
@@ -0,0 +1,49 @@
+const settings = {
+  "name": "react-blog",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Blog",
+              "/blog"
+            ],
+            [
+              "About",
+              "/about/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          "url": "https://omwtotech.wordpress.com/"
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
